7ddbd26e5bd1629e04acca85458d0f5b28ea720c
rmorgri0107 <rmorgri0107@g.educaand.es>
2024-11-15 12:16:55 +0100

    Crear carpeta ejercicios y archivos

--- aaaaaaa.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Introduce un numero"))
-
-seguir = True
-while seguir:
-    if n != 0:
-        n = int(input("Introduzca otro numero"))
-         
-    else:
-      seguir = False
--- ejercicios1/prueba1.py
+++ ejercicios1/prueba1.py
@@ -3,3 +3,4 @@ if edad >= 18:
     print("Toma una cerveza!")  
 else:  
     print(f"Toma un zumo de piña, con {edad} años eres menor.")
+    
